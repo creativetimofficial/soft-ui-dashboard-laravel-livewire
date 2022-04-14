01174a7a09b0b7c69879b87f1d93ffb66416a46c
ovidiustanc123 <ovidiu.stanc@updivision.com>
2022-04-14 12:55:17 +0300

    nav-title-update

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -3,9 +3,9 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="{{ route('dashboard') }}">
+        <a class="align-items-center d-flex m-0 navbar-brand text-wrap" href="{{ route('dashboard') }}">
             <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="...">
-            <span class="ms-1 font-weight-bold">Soft UI Dashboard</span>
+            <span class="ms-1 font-weight-bold">Soft UI Dashboard Laravel Livewire</span>
         </a>
     </div>
     <hr class="horizontal dark mt-0">
--- resources/views/layouts/navbars/guest/login.blade.php
+++ resources/views/layouts/navbars/guest/login.blade.php
@@ -2,7 +2,7 @@
     class="navbar navbar-expand-lg  blur blur-rounded top-0  z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
     <div class="container-fluid">
         <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="{{ route('dashboard') }}">
-            Soft UI Dashboard
+            Soft UI Dashboard Laravel Livewire
         </a>
         <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
             data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
