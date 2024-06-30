31bbe2b8ee5c57094092ce76f6774662c8f3c591
pipobhos <pichapobhos@gmail.com>
2024-06-30 09:00:33 +0700

    fix tooltip not show

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -61,6 +61,7 @@
             Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
         }
     </script>
+
     <!-- Github buttons -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
--- resources/views/livewire/hosxp-lab-result.blade.php
+++ resources/views/livewire/hosxp-lab-result.blade.php
@@ -1,9 +1,11 @@
 <p wire:key="lab-r-detail-{{ $labnumber }}-{{$vn}}" class="text-xs font-weight-bold m-0 p-0"  wire:lazy>
-    <button type="button" class="btn btn-sm bg-gradient-primary btn-tooltip my-1 py-1" data-bs-toggle="tooltip" data-bs-placement="top"
+    <button x-data="{}" x-init="new bootstrap.Tooltip($el)"
+    type="button" class="btn btn-sm bg-gradient-primary btn-tooltip my-1 py-1" data-bs-toggle="tooltip" data-bs-placement="top"
     @if($labResultDetail->isNotEmpty())
         title="{{ $labResultDetail->pluck('lab_items_name_ref')->implode(' - ') }}"
     @else
         title="No records found."
     @endif
+
     data-container="body" data-animation="true">อ่านผล {{$labnumber}}</button>
 </p>
